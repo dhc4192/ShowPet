57af6d157e120a0091187ea2594103db0277bd25
Daehyun <dhc4192@gmail.com>
2020-06-09 17:34:58 -0400

    completed services folder in the frontend

---
+++ client/src/services/pets.js
@@ -0,0 +1,31 @@
+import api from "./api-helper";
+
+export const getAllPets = async () => {
+  const resp = await api.get("/pets");
+  return resp.data;
+};
+
+export const getOnePet = async (id) => {
+  const resp = await api.get(`/pet/${id}`);
+  return resp.data;
+};
+
+export const createPet = async (petData) => {
+  const resp = await api.post("/pets", { pet: petData });
+  return resp.data;
+};
+
+export const updatePet = async (id, petData) => {
+  const resp = await api.put(`/pet/${id}`, { pet: petData });
+  return resp.data;
+};
+
+export const deletePet = async (id) => {
+  const resp = await api.delete(`/pet/${id}`);
+  return resp.data;
+};
+
+export const petToCategory = async (petId, categoryId) => {
+  const resp = await api.get(`/categories/${categoryId}/pets/${petId}`);
+  return resp.data;
+};
