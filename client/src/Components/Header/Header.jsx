995bf4edc92f4f09668360cf2daae24afc446af4
Daehyun <dhc4192@gmail.com>
2020-06-11 19:58:55 -0400

    finished the css for header 1

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ShowPetsLogo from "../../Assets/Images/ShowPets-Logo-small.png";
 import AddIcon from "../../Assets/Images/Add-Icon.png";
+import "./Header.css";
 
 export default function Header(props) {
   return (
@@ -10,27 +11,30 @@ export default function Header(props) {
         <img className="header-logo" src={ShowPetsLogo} alt="ShowPet Logo" />
         <p className="header-title">ShowPets</p>
       </div>
+      <div className="header-two">
+        {props.currentUser ? (
+          <>
+            <p className="header-welcome-user">
+              Welcome, {props.currentUser.username}
+            </p>
+            <Link to="/">
+              <button
+                className="header-sign-out-button"
+                onClick={props.handleSignOut}
+              >
+                Sign Out
+              </button>
+            </Link>
+          </>
+        ) : null}
 
-      {props.currentUser ? (
-        <div className="header-two">
-          Welcome, {props.currentUser.username}
-          <Link to="/">
-            <button
-              className="header-sign-out-button"
-              onClick={props.handleSignOut}
-            >
-              Sign Out
-            </button>
-          </Link>
-        </div>
-      ) : null}
-
-      <Link to="/add/pet">
-        <button className="header-add-button">
-          <img className="header-add-icon" src={AddIcon} />
-          Add Pet
-        </button>
-      </Link>
+        <Link to="/add/pet">
+          <button className="header-add-button">
+            <img className="header-add-icon" src={AddIcon} />
+            Add Pet
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
