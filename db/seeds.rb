3b95339097d6b3099c0e2d579a2a2231a78aebf7
Daehyun <dhc4192@gmail.com>
2020-06-12 16:57:20 -0400

    updated data

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,6 +26,14 @@ User.destroy_all
     password: 'dhc4192'
   }
 )
+@user3 = User.create!(
+  {
+    username: 'D',
+    email: 'd@gmail.com',
+    password: 'dhc4192'
+  }
+)
+
 p "#{User.count} user(s) created"
 
 @dog = Category.create!(name: 'Dog')
@@ -38,13 +46,94 @@ p "#{User.count} user(s) created"
 p "#{Category.count} category created"
 
 Pet.create!(
-  name: 'Kid',
-  image: 'https://i.ibb.co/wKBYhXQ/Kid1.jpg',
-  breed: 'Maltese',
+  name: 'Simba',
+  image: 'https://i.ibb.co/ZLGCm53/01-lion-populations-nationalgeographic-1777804.jpg',
+  breed: 'Lion',
+  age: 10,
+  description: '.......',
+  user: @user3,
+  category: @other
+)
+Pet.create!(
+  name: 'Fil-A',
+  image: 'https://ibb.co/gTbXVRT',
+  breed: 'Chicken',
+  age: 3,
+  description: 'Getting ready.',
+  user: @user3,
+  category: @bird
+)
+Pet.create!(
+  name: 'Chip',
+  image: 'https://i.ibb.co/XXDKqHZ/parakeet.jpg',
+  breed: 'Parakeet',
+  age: 2,
+  description: 'Just chilling.',
+  user: @user2,
+  category: @bird
+)
+Pet.create!(
+  name: 'Sonic',
+  image: 'https://i.ibb.co/44Xc1RC/hedge.jpg',
+  breed: 'Hedgehog',
+  age: 1,
+  description: "Doesn't seem too fast.",
+  user: @user2,
+  category: @exotic
+)
+Pet.create!(
+  name: 'Mini Ham',
+  image: 'https://i.ibb.co/10t2wkC/ham2.jpg',
+  breed: 'Roborovski Dwarf Hamster',
+  age: 1,
+  description: 'So tiny.',
+  user: @user2,
+  category: @hamster
+)
+Pet.create!(
+  name: 'Thief',
+  image: 'https://i.ibb.co/rpcDD07/racoon.jpg',
+  breed: 'Raccoon',
+  age: 2,
+  description: 'Just cruising around.',
+  user: @user3,
+  category: @other
+)
+Pet.create!(
+  name: 'Ham',
+  image: 'https://i.ibb.co/RQfVPsL/ham1.jpg',
+  breed: 'Syrian Hamster',
+  age: 1,
+  description: 'WAIT!.',
+  user: @user2,
+  category: @hamster
+)
+Pet.create!(
+  name: 'Damn',
+  image: 'https://i.ibb.co/9Hs3dBS/fennec.jpg',
+  breed: 'Fenec Fox',
+  age: 3,
+  description: 'Them ears though.',
+  user: @user2,
+  category: @exotic
+)
+Pet.create!(
+  name: 'Boss',
+  image: 'https://i.ibb.co/KVt2xmr/bluenose.jpg',
+  breed: 'Bluenose Pit',
   age: 6,
-  description: 'Loves being on the baby swings.',
-  user: @user1,
+  description: 'Look at that handsomeness',
+  user: @user2,
   category: @dog
+)
+Pet.create!(
+  name: 'Rush',
+  image: 'https://i.ibb.co/ZBczLc5/russian.jpg',
+  breed: 'Russian Blue',
+  age: 4,
+  description: 'What you looking at?',
+  user: @user2,
+  category: @cat
 )
 Pet.create!(
   name: 'Kid',
@@ -57,12 +146,21 @@ Pet.create!(
 )
 Pet.create!(
   name: 'Lam',
-  image: 'https://i.ibb.co/2Zj3vyB/Lam.jpg',
+  image: 'https://i.ibb.co/ZNpHxWJ/Lam.jpg',
   breed: 'American Shorthair',
   age: 3,
   description: 'Getting her beauty sleep.',
-  user: @user2,
+  user: @user1,
   category: @cat
+)
+Pet.create!(
+  name: 'Kid',
+  image: 'https://i.ibb.co/XzHVLzG/Kid1.jpg',
+  breed: 'Maltese',
+  age: 6,
+  description: 'Loves being on the baby swings.',
+  user: @user1,
+  category: @dog
 )
 
 p "#{Pet.count} pet(s) created"
